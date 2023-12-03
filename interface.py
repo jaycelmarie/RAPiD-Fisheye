88f8a42fa9db9164418f3fb77e95312403a005ca
jaycelmarie <C20372876@mytudublin.ie>
2023-12-03 00:15:05 +0000

    Added viewimages and camera defs

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 import customtkinter
+from api import Detector
+
 
 # define parameters, apperance
 customtkinter.set_appearance_mode("dark")
@@ -8,8 +10,19 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 root.geometry("500x350")
 
-def gui():
-    print("Test")
+def viewimages():
+    # Initialize detector
+    detector = Detector(model_name='rapid',
+                        weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt',
+                        use_cuda=False)
+
+    # A simple example to run on a single image and plt.imshow() it
+    detector.detect_one(img_path='./images/exhibition.jpg',
+                        input_size=1024, conf_thres=0.3,
+                        visualize=True)
+    
+def camera():
+    print("You have chosen to Open Camera.")
 
 
 # Add Frame
@@ -19,7 +32,10 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 label = customtkinter.CTkLabel(master=frame, text="User Interface")
 label.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Image", command=gui)
+openCamera = customtkinter.CTkButton(master=frame, text="Open Camera", command=camera)
+openCamera.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Image", command=viewimages)
 button.pack(pady=12, padx=10)
 
 root.mainloop()
