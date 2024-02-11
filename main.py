8e6d7cd306f0e78d91f1903ad39419fad2c3a4c9
jaycelmarie <C20372876@mytudublin.ie>
2024-02-11 22:59:32 +0000

    Added images and changes to path

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def rapidfa():
                         use_cuda=False)
 
     # A simple example to run on a single image and plt.imshow() it
-    detector.detect_one(img_path='./examples/warehouse_short/warehouse_000451.png',
+    detector.detect_one(img_path='./images/warehouse_short/warehouse_000451.png',
                         input_size=1024, conf_thres=0.3,
                         visualize=True)
 
@@ -50,7 +50,7 @@ def rapidfgfa():
                         use_cuda=False)
 
     # A simple example to run on a single image and plt.imshow() it
-    detector.detect_one(img_path='./examples/warehouse_short/warehouse_000451.png',
+    detector.detect_one(img_path='./images/warehouse_short/warehouse_000451.png',
                         input_size=1024, conf_thres=0.3,
                         visualize=True)
 
