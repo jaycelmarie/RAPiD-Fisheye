2cae0f87da6d02ae7ec99476f325da93b228c72f
jaycelmarie <C20372876@mytudublin.ie>
2024-02-11 23:19:53 +0000

    revert paddingy and paddingx, test height and width

--- main.py
+++ main.py
@@ -68,21 +68,21 @@ frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 label = customtkinter.CTkLabel(master=frame, text="Welcome")
-label.pack(pady=15, padx=10)
+label.pack(pady=12, padx=10)
 
-openCameraBtn = customtkinter.CTkButton(master=frame, text="Camera", command=camera, height=100, width=100)
-openCameraBtn.pack(pady=15, padx=12)
+openCameraBtn = customtkinter.CTkButton(master=frame, text="Camera", command=camera, height=50, width=120)
+openCameraBtn.pack(pady=12, padx=10)
 
 rapidBtn = customtkinter.CTkButton(master=frame, text="RAPID Image", command=rapid)
-rapidBtn.pack(pady=15, padx=12)
+rapidBtn.pack(pady=12, padx=10)
 
 faBtn = customtkinter.CTkButton(master=frame, text="RAPID-FA Image", command=rapidfa)
-faBtn.pack(pady=15, padx=12)
+faBtn.pack(pady=12, padx=10)
 
 fgfaBtn = customtkinter.CTkButton(master=frame, text="RAPID-FGFA Image", command=rapidfgfa)
-fgfaBtn.pack(pady=15, padx=12)
+fgfaBtn.pack(pady=12, padx=10)
 
 exitBtn = customtkinter.CTkButton(master=frame, text="Exit", command=leave)
-exitBtn.pack(pady=15, padx=12)
+exitBtn.pack(pady=12, padx=10)
 
 root.mainloop()
