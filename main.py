0d10de5352a77b896abe4f13acf755903af2f583
jaycelmarie <C20372876@mytudublin.ie>
2024-02-13 19:24:52 +0000

    Test for camera live

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ def camera():
 
         cv2.imshow('video', frame)
 
+        # Initialize detector
+        detector = Detector(model_name='rapid',
+                            weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt',
+                            use_cuda=False)
+        
+        # A simple example to run on a single image and plt.imshow() it
+        detector.detect_one(img_path=frame,
+                            input_size=1024, conf_thres=0.3,
+                            visualize=True)
+
         if cv2.waitKey(20) == ord("q"):
             break
 
